037b883e621b983a4480af25b8ab76186c40b2e5
Todd Mancini <todd.mancini@daxat.com>
2019-03-18 18:14:58 -0400

    Update docker-entrypoint.sh
    Fml

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -134,7 +134,7 @@ else
 		echo "location / {" >> /etc/nginx/sites-enabled/webapp.conf
 		echo "        proxy_pass          $THIS_DEST;" >> /etc/nginx/sites-enabled/webapp.conf
 		envsubst '$PROXY_PORT' < /etc/nginx/sites-available/webapp.2.conf >> /etc/nginx/sites-enabled/webapp.conf
-		echo "}"
+		echo "}" >> /etc/nginx/sites-enabled/webapp.conf
 		
 		if [ "$REDIRECT_WWW_TO_ROOT" = true ]; then
 			echo "Adding www redirect for $i"
